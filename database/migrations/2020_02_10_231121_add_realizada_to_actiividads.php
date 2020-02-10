e470b1a45d28bab0a81a526da0bc3439a81029df
Alejandro Gzz <adgm1988@gmail.com>
2020-02-10 17:31:00 -0600

    actividad realizada y created y edited by

--- database/migrations/2020_02_10_231121_add_realizada_to_actiividads.php
+++ database/migrations/2020_02_10_231121_add_realizada_to_actiividads.php
@@ -14,6 +14,11 @@ class AddRealizadaToActiividads extends Migration
     public function up()
     {
         Schema::table('actividads', function (Blueprint $table) {
+            $table->unsignedBigInteger('created_by');
+            $table->foreign('created_by')->references('id')->on('users');
+
+            $table->unsignedBigInteger('edited_by');
+            $table->foreign('edited_by')->references('id')->on('users');
 
             $table->boolean('realizada');
         });
