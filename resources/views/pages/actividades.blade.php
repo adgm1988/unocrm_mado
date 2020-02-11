50b33401883c92a57246b3e3cfeff28897e1d178
Alejandro Gzz <adgm1988@gmail.com>
2020-02-10 18:25:42 -0600

    cambiar palabra resultado por comentraios

--- resources/views/pages/actividades.blade.php
+++ resources/views/pages/actividades.blade.php
@@ -122,7 +122,7 @@
 					@if(auth::user()->admin ==1 || auth::user()->consultor ==1)
 					<div class="row">
 						<div class="form-group col-md-12">
-							<label for="resultado">Resultado:</label>
+							<label for="resultado">Comentarios:</label>
 							<input type="text" class="form-control" name="resultado">
 						</div>
 					</div>
