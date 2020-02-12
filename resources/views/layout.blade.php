947765fb4d8b03ba379e3d5ed25a1ad878661ecd
Alejandro Gzz <adgm1988@gmail.com>
2020-02-12 11:21:35 -0600

    cambio title en layout

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -8,7 +8,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>{{ config('app.name', 'UnoCrm') }}</title>
+    <title>{{ config('app.name', 'GumonCrm') }}</title>
     <!-- Custom fonts for this template-->
     <link href="{{ asset('vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
     <!-- Page level plugin CSS-->
