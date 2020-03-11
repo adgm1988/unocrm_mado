894ef82bd10364a5d7cc58fdd607a9750c7c2233
Alejandro Gzz <adgm1988@gmail.com>
2020-03-11 00:06:25 -0600

    se agrego la migracion de archivos

---
+++ database/migrations/2020_03_11_000124_create_quotes_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateQuotesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('quotes', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('prospecto_id');
+            $table->foreign('prospecto_id')->references('id')->on('prospectos');
+            $table->string('name');
+            $table->date('fecha');
+            $table->double('monto');
+            $table->text('descripcion');
+            $table->string('ruta_archivo');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('quotes');
+    }
+}
